0d302329efb482023b29c2ffe8d105412e1e5548
Miguel Alejandro Salgado Ramírez <alejandrosram@outlook.com>
2022-07-21 17:53:36 -0500

    Update jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
-export default async () => {
-	return {
-		verbose: true,
-		testEnvironment: "jest-environment-jsdom",
-		setupFiles: ["./jest.setup.js"],
-	};
+module.exports = {
+	testEnvironment: "jest-environment-jsdom",
+	setupFiles: ["./jest.setup.js"],
 };
